836f10c886299a97b5a5c2817f9dd3c0162d33ad
LaRosaM <jose.larosa6@unmsm.edu.pe>
2019-10-26 17:49:34 -0500

    Finalizado modulo 3 - Django
    Finalizado el modulo 3 del tutorial de Django

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('questions/', views.questions, name='questions'),
+    path('questions/list', views.questions_list, name='questions_list'),
     path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
